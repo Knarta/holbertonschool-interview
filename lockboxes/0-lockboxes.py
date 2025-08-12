c733d98ec7cbc7e897258c50633a402789e22e74
knarta <charlene.martin@holbertonstudents.com>
2025-08-12 16:44:10 +0200

    Add function to check if all lockboxes can be unlocked

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """
+    Check if we can unlock all the boxes
+    """
+    if not boxes:
+        return False
+
+    n = len(boxes)
+    unlocked_boxes = [False] * len(boxes)
+    unlocked_boxes[0] = True  # first box is open
+
+    keys = [0]  # start with first box
+
+    while keys:
+        box = keys.pop()
+        for key in boxes[box]:
+            if key < len(boxes) and not unlocked_boxes[key]:
+                unlocked_boxes[key] = True
+                keys.append(key)
+
+    return all(unlocked_boxes)
