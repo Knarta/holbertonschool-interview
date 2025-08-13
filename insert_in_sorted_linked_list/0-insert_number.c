af58acdf7f1695b761b25c50487f7d99abb8bff7
Knarta <charlene.martin@holbertonstudents.com>
2025-08-13 13:02:00 +0200

    Refactor insert_node function in sorted linked list: Improve insertion logic to handle edge cases and maintain sorted order by updating pointers correctly.

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -9,27 +9,32 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new_node;
+	listint_t *new_node, *current, *prev;
 
 	new_node = malloc(sizeof(listint_t));
-
 	if (new_node == NULL)
-	{
 		return (NULL);
-	}
 
 	new_node->n = number;
 	new_node->next = NULL;
 
-	if (*head == NULL)
+	if (*head == NULL || number < (*head)->n)
 	{
-		(*head) = new_node;
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
-	else
+
+	current = *head;
+	prev = NULL;
+	while (current != NULL && current->n < number)
 	{
-		new_node->next = (*head);
-		(*head) = new_node;
+		prev = current;
+		current = current->next;
 	}
+
+	prev->next = new_node;
+	new_node->next = current;
 
 	return (new_node);
 }
