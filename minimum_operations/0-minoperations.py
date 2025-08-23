9516c1805fcadd93c45146283d3ec11000b97817
Knarta <charlene.martin@holbertonstudents.com>
2025-08-23 19:14:11 +0200

    Add function to calculate minimum operations: Implement minOperations to determine the fewest operations needed to achieve exactly n 'H' characters, including logic for clipboard management and current string length tracking.

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,7 +1,31 @@
 #!/usr/bin/python3
 """
-Module 
+Method that calculates the fewest number of operations needed to
+ result in exactly n H characters in the file.
+
+operation_needed : count operations
+clipboard: store currentclipboard contents
+current_length: track the current length of the string
+
 """
 
 
 def minOperations(n):
+
+    if n <= 1:
+        return 0
+
+    operation_needed = 0
+    clipboard = 0
+    current_length = 1
+
+    while n > current_length:
+        if n % current_length == 0:
+            clipboard = current_length
+            operation_needed += 1
+
+        else:
+            current_length += clipboard
+            operation_needed += 1
+
+    return operation_needed
