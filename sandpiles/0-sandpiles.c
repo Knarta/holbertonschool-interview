5f9560a53648dfc504445e3d3673c7b827061a3e
Knarta <charlene.martin@holbertonstudents.com>
2025-09-18 12:10:25 +0200

    add correction betty stylz

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -9,17 +9,70 @@
  */
 static int is_stable(int grid[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (grid[i][j] > 3)
-                return (0);
-        }
-    }
-    return (1);
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid[i][j] > 3)
+				return (0);
+		}
+	}
+	return (1);
+}
+
+/**
+ * print_sandpile - Print a sandpile grid
+ * @grid: 3x3 grid to print
+ */
+static void print_sandpile(int grid[3][3])
+{
+	int i, j;
+
+	printf("=\n");
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+/**
+ * stabilize_sandpile - Stabilize an unstable sandpile
+ * @grid: 3x3 grid to stabilize
+ */
+static void stabilize_sandpile(int grid[3][3])
+{
+	int i, j, temp[3][3];
+
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			temp[i][j] = grid[i][j];
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (temp[i][j] > 3)
+			{
+				grid[i][j] -= 4;
+				if (i > 0)
+					grid[i - 1][j] += 1;
+				if (i < 2)
+					grid[i + 1][j] += 1;
+				if (j > 0)
+					grid[i][j - 1] += 1;
+				if (j < 2)
+					grid[i][j + 1] += 1;
+			}
+		}
+	}
 }
 
 /**
@@ -29,59 +82,15 @@ static int is_stable(int grid[3][3])
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    int i, j;
-    int temp[3][3];
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            grid1[i][j] += grid2[i][j];
-        }
-    }
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			grid1[i][j] += grid2[i][j];
 
-
-    while (!is_stable(grid1))
-    {
-        printf("=\n");
-        for (i = 0; i < 3; i++)
-        {
-            for (j = 0; j < 3; j++)
-            {
-                if (j)
-                    printf(" ");
-                printf("%d", grid1[i][j]);
-            }
-            printf("\n");
-        }
-
-
-        for (i = 0; i < 3; i++)
-        {
-            for (j = 0; j < 3; j++)
-            {
-                temp[i][j] = grid1[i][j];
-            }
-        }
-
-
-        for (i = 0; i < 3; i++)
-        {
-            for (j = 0; j < 3; j++)
-            {
-                if (temp[i][j] > 3)
-                {
-                    grid1[i][j] -= 4;
-                    if (i > 0)
-                        grid1[i - 1][j] += 1;
-                    if (i < 2)
-                        grid1[i + 1][j] += 1;
-                    if (j > 0)
-                        grid1[i][j - 1] += 1;
-                    if (j < 2)
-                        grid1[i][j + 1] += 1;
-                }
-            }
-        }
-    }
+	while (!is_stable(grid1))
+	{
+		print_sandpile(grid1);
+		stabilize_sandpile(grid1);
+	}
 }
