553580dfd607eedf9590fe23d0603d2fbe754cf1
knarta <charquen10@gmail.com>
2025-11-24 17:44:45 +0100

    Refactor character fetching logic in 0-starwars_characters.js: Update arrow functions to traditional function expressions for consistency, and modify string concatenation for improved readability.

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 
 const request = require('request');
 
@@ -9,16 +9,16 @@ if (!movieId) {
   process.exit(1);
 }
 
-const filmUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+const filmUrl = 'https://swapi-api.hbtn.io/api/films/' + movieId + '/';
 
-request(filmUrl, (error, response, body) => {
+request(filmUrl, function (error, response, body) {
   if (error) {
     console.error('Error:', error);
     return;
   }
 
   if (response.statusCode !== 200) {
-    console.error(`Error: Status code ${response.statusCode}`);
+    console.error('Error: Status code ' + response.statusCode);
     return;
   }
 
@@ -32,8 +32,8 @@ request(filmUrl, (error, response, body) => {
   let completed = 0;
   const characterNames = new Array(characters.length);
 
-  characters.forEach((characterUrl, index) => {
-    request(characterUrl, (err, res, charBody) => {
+  characters.forEach(function (characterUrl, index) {
+    request(characterUrl, function (err, res, charBody) {
       if (err) {
         console.error('Error:', err);
         return;
@@ -44,7 +44,7 @@ request(filmUrl, (error, response, body) => {
       completed++;
 
       if (completed === characters.length) {
-        characterNames.forEach(name => {
+        characterNames.forEach(function (name) {
           console.log(name);
         });
       }
